dd0c9a171b448487aff5363958471020ae04bfd8
TrigrD3 <TrigrD3@users.noreply.github.com>
2022-07-02 12:49:09 +0800

    fix footer responsive

--- BPR-CI/project-root/app/Controllers/Pages.php
+++ BPR-CI/project-root/app/Controllers/Pages.php
@@ -11,62 +11,9 @@ class Pages extends BaseController
             'css' => 'index',
             'font' => 'font',
             'navbar' => 'navbar',
+            'footer' => 'footer',
         ];
         echo view('pages/index', $data);
     }
 
-    public function login()
-    {
-        $data = [
-            'title' => 'Login Page | Credit Scoring',
-            'css' => 'login',
-            'font' => 'font',
-            'navbar' => 'navbar',
-        ];
-        echo view('pages/login', $data);
-    }
-
-    public function FormAkun()
-    {
-        $data = [
-            'title' => 'FormAkun Page | Credit Scoring',
-            'css' => 'FormAkun',
-            'font' => 'font',
-            'navbar' => 'navbar',
-        ];
-        echo view('pages/FormAkun', $data);
-    }
-
-    public function hasil()
-    {
-        $data = [
-            'title' => 'Hasil Page | Credit Scoring',
-            'css' => 'hasil',
-            'font' => 'font',
-            'navbar' => 'navbar',
-        ];
-        echo view('pages/hasil', $data);
-    }
-
-    public function inputData5C()
-    {
-        $data = [
-            'title' => 'Input Data 5C | Credit Scoring',
-            'css' => 'inputData5C',
-            'font' => 'font',
-            'navbar' => 'navbar',
-        ];
-        echo view('pages/inputData5C', $data);
-    }
-
-    public function UbahBobotSkorCharacter()
-    {
-        $data = [
-            'title' => 'Ubah Bobot Skor Character | Credit Scoring',
-            'css' => 'UbahBobotSkorCharacter',
-            'font' => 'font',
-            'navbar' => 'navbar',
-        ];
-        echo view('pages/UbahBobotSkorCharacter', $data);
-    }
 }
--- BPR-CI/project-root/app/Views/layout/template.php
+++ BPR-CI/project-root/app/Views/layout/template.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/<?= $css; ?>.css">
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/<?= $font; ?>.css">
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/<?= $navbar; ?>.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/<?= $footer; ?>.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <!-- Bootstrap Font Icon CSS -->
@@ -30,16 +31,9 @@
 
     <?= $this->renderSection('content'); ?>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+
     <?= $this->include('layout/footer'); ?>
 </body>
 <!-- JavaScript Bundle with Popper -->
