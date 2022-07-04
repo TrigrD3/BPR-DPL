a3d24db816ee95a57c4c8067c3fc169a6a6a2016
TrigrD3 <TrigrD3@users.noreply.github.com>
2022-07-04 23:54:56 +0800

    fix header tulisan

--- BPR-CI/project-root/app/Views/layout/header.php
+++ BPR-CI/project-root/app/Views/layout/header.php
@@ -11,7 +11,10 @@
 <header>   
 <nav class="navbar navbar-expand-lg bg-light ">
   <div class="container">
-    <a class="navbar-brand" href="/"><img src="<?php echo base_url('images/logoBPR.png'); ?>" >BPR Lestari <br>PT BPR Danapermata Lestari</a>
+  <a class="navbar-brand text-dark d-flex" href="#">
+            <img src="<?php echo base_url('images/logoBPR.png'); ?>" alt="Logo BPR">
+            <span class="ml-1">BPR Lestari <br>PT BPR Danapermata Lestari</span>
+        </a>
     <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon "></span>
     </button>
