a8030235793c35e2ebff1e517786d9d6af1587d8
SeanRcd <vincentius.sean@gmail.com>
2022-07-13 13:27:28 +0800

    update alamat

--- BPR-CI/project-root/app/Views/layout/navbar.php
+++ BPR-CI/project-root/app/Views/layout/navbar.php
@@ -35,7 +35,7 @@
                 <li><a class="dropdown-item" href="Profil">Profil Bank</a></li>
                 <li><a class="dropdown-item" href="VisiMisi">Visi Dan Misi</a></li>
                 <li><a class="dropdown-item" href="StrukturOrganisasi">Struktur Organisasi</a></li>
-                <li><a class="dropdown-item" href="#">Alamat Kantor</a></li>
+                <li><a class="dropdown-item" href="AlamatKantor">Alamat Kantor</a></li>
                 <li><a class="dropdown-item" href="PusatBerita">Berita Lestari</a></li>
             </ul>
         </div>
