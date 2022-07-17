e2d57720e2e642e25a499916e97b0d69b6ffba45
TrigrD3 <TrigrD3@users.noreply.github.com>
2022-07-17 16:58:30 +0800

    fix lelang gmap onclick

--- BPR-CI/project-root/public/assets/js/lelang.js
+++ BPR-CI/project-root/public/assets/js/lelang.js
@@ -1,21 +1,10 @@
 const thumbs = document.querySelector('.thumb-img').children;
-const gmap = document.querySelector('.gmap-main').children;
 
-function changeImage(event) {
+function changeContent(event) {
   document.querySelector('.pro-img').src = event.children[0].src;
-
+  document.querySelector('.gmap').src = event.children[1].src;
   for (let i = 0; i < thumbs.length; i++) {
     thumbs[i].classList.remove('active');
   }
-
-  event.classList.add('active');
-}
-
-function changeGmap(event) {
-  document.querySelector('.gmap-list').src = event.children[0].src;
-  for (let i = 0; i < gmap.length; i++) {
-    gmap[i].classList.remove('active');
-  }
-
   event.classList.add('active');
 }
