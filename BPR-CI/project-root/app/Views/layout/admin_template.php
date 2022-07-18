2a103de23d91659c321a00bada02faf6a408a37e
Yusuf <1910817210003@mhs.ulm.ac.id>
2022-07-19 02:10:42 +0800

    UPDATE FOTO

--- BPR-CI/project-root/app/Views/layout/admin_template.php
+++ BPR-CI/project-root/app/Views/layout/admin_template.php
@@ -28,8 +28,7 @@
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
-
+    <div class="wrapper">Z
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="assets/img/logoBPR.png" alt="AdminLTELogo" height="60" width="60">
