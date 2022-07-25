12be9a16664e2e5f25d0d9e36f4c9a9e1ce509ab
Yusuf <1910817210003@mhs.ulm.ac.id>
2022-07-25 22:41:55 +0800

    uodate

--- app/Controllers/LayananLainController.php
+++ app/Controllers/LayananLainController.php
@@ -29,117 +29,23 @@ class LayananLainController extends BaseController
         return view('Admin/LayananLain/AdminLayananLainnya', $data);
     }
 
-    public function TambahDeskripsiLL()
+    public function TambahPembayaran()
     {
         $data = [
-            'title' => 'Edit Deskripsi Layanan Lainnya',
+            'title' => 'Edit',
             'css' => 'Style',
             'font' => 'font',
         ];
-        echo view('admin/LayananLain/TambahDeskripsi', $data);
+        echo view('admin/Kredit/TambahProdukKredit', $data);
     }
-    public function TambahFotoLL()
+
+    public function TambahIklanKredit()
     {
         $data = [
-            'title' => 'Tambah Foto Layanan Lainnya',
+            'title' => 'Admin Kredit',
             'css' => 'Style',
             'font' => 'font',
         ];
-        echo view('admin/LayananLain/TambahFoto', $data);
-    }
-    public function AddDeskripsiLL()
-    {
-        $data = [
-            'deskripsi' => $this->request->getPost('deskripsi'),
-        ];
-
-        $this->LayananLainModel->add_deskripsill($data);
-        session()->setFlashdata('message', 'Tambah Layanan Lainnya Berhasil');
-        return redirect()->to(base_url('AdminLayananLainnya'))->with('success', 'Data Added Successfully');
-    }
-
-    public function AddFotoLL()
-    {
-        $data = [
-            'foto' => $this->request->getPost('foto'),
-        ];
-
-        $this->LayananLainModel->add_fotoll($data);
-        session()->setFlashdata('message', 'Tambah Layanan Lainnya Berhasil');
-        return redirect()->to(base_url('AdminLayananLainnya'))->with('success', 'Data Added Successfully');
-    }
-
-    public function EditDeskripsiLL($id)
-    {
-        $dataAll = $this->LayananLainModel->get_id_layananlain($id);
-        if (empty($dataAll)) {
-            throw new \CodeIgniter\Exceptions\PageNotFoundException('Data Produk Layanan Lainnya Tidak ditemukan !');
-        }
-        $data['deskripsi_layananlain'] = $dataAll;
-        echo view('admin/LayananLain/EditDeskripsiLL', $data);
-    }
-
-    public function EditFotoLL($id)
-    {
-        $dataAll = $this->LayananLainModel->get_id_foto($id);
-        if (empty($dataAll)) {
-            throw new \CodeIgniter\Exceptions\PageNotFoundException('Data Produk Iklan Layanan Lainnya Tidak ditemukan !');
-        }
-        $data['foto_layananlain'] = $dataAll;
-        echo view('admin/LayananLain/EditFotoLL', $data);
-    }
-
-    public function UpdateDeskripsiLL($id)
-    {
-        $data = [
-            'deskripsi' => $this->request->getPost('editor1'),
-        ];
-
-        $this->LayananLainModel->update_layananlain($id, $data);
-
-        $this->session->setFlashdata('message', '<div class="alert alert-warning" role="alert">Data berhasil diedit.
-            <button class="close" type="button" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>');
-        return redirect()->to(base_url('AdminLayananLainnya'));
-    }
-
-    public function UpdateFotoLL($id)
-    {
-        $data = [
-            'foto' => $this->request->getPost('foto'),
-        ];
-
-        $this->LayananLainModel->update_id_foto($id, $data);
-
-        $this->session->setFlashdata('message', '<div class="alert alert-warning" role="alert">Data berhasil diedit.
-            <button class="close" type="button" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>');
-        return redirect()->to(base_url('AdminLayananLainnya'));
-    }
-
-    public function DeleteLayananLain($id)
-    {
-        $data = $this->LayananLainModel->get_id_layananlain($id);
-        if (empty($data)) {
-            throw new \CodeIgniter\Exceptions\PageNotFoundException('Layanan Lain Tidak ditemukan !');
-        }
-        $this->LayananLainModel->delete_layananlain($id);
-        session()->setFlashdata('message', 'Hapus Layanan Lain Berhasil');
-        return redirect()->to(base_url('AdminLayananLainnya'));
-    }
-
-    public function DeleteIklanLayananLainnya($id)
-    {
-        $data = $this->LayananLainModel->get_id_iklan($id);
-        if (empty($data)) {
-            throw new \CodeIgniter\Exceptions\PageNotFoundException('Layanan Lainnya Tidak ditemukan !');
-        }
-        $this->LayananLainModel->delete_Iklandeposito($id);
-        session()->setFlashdata('message', 'Hapus Iklan Deposito Berhasil');
-        return redirect()->to(base_url('AdminLayananLainnya'));
+        echo view('admin/kredit/TambahIklanKredit', $data);
     }
 }
